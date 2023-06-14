ee21558d27a475cd753f2c324a99a6c099b91ce1
BACoburn93 <bcoburn1@frontiernet.net>
2023-06-14 18:13:26 -0400

    have ball bounce on x and y plane

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -9,19 +9,37 @@ const GameCanvas = () => {
         let ballX = 75;
         let ballSpeedX = 5;
         let ballY = 75;
+        let ballSpeedY = 10;
 
         const canvas = canvasRef.current;
         const canvasContext = canvas.getContext('2d');
 
         const updateAll = () => {
             ballX += ballSpeedX;
+            ballY += ballSpeedY;
+
+            if(ballX > canvas.width) {
+                ballSpeedX = -ballSpeedX;
+            }
+
+            if(ballX < 0) {
+                ballSpeedX = -ballSpeedX;
+            }
+
+            if(ballY > canvas.height) {
+                ballSpeedY = -ballSpeedY;
+            }
+
+            if(ballY < 0) {
+                ballSpeedY = -ballSpeedY;
+            }
 
             canvasContext.fillStyle = '#000000';
             canvasContext.fillRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
 
             canvasContext.fillStyle = '#FFF';
             canvasContext.beginPath();
-            canvasContext.arc(ballX, 100, 10, 0, Math.PI*2, true);
+            canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true);
             canvasContext.fill();
           }
 

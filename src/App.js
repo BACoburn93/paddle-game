2fac5ad611ef21ad0dc97b7e33c6034863c63e03
BACoburn93 <bcoburn1@frontiernet.net>
2023-06-14 17:33:35 -0400

    start game

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GameCanvas from "./components/GameCanvas/game-canvas.component";
+
+function App() {
+  return (
+    <div className="App">
+      <GameCanvas />
+    </div>
+  );
+}
+
+export default App;

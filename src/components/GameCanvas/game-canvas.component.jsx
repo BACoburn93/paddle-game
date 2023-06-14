e0d1b39feb2d77a2ca4429216c3968c6e2524347
BACoburn93 <bcoburn1@frontiernet.net>
2023-06-14 17:57:44 -0400

    set up canvas in react

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -1,9 +1,22 @@
+import React, { useRef, useEffect } from 'react'
 
+import './game-canvas.styles.scss';
 
 const GameCanvas = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.fillStyle = '#000000';
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+      }, [])
+
     return (
         <div>
-            <h1>Hello</h1>
+            <canvas id='gameCanvas' width='800' height='600' ref={canvasRef}>
+
+            </canvas>
         </div>
     )
 }

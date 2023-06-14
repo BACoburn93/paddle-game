fc136b25b1e69749a51c57c3fdb78c84058725b0
BACoburn93 <bcoburn1@frontiernet.net>
2023-06-14 18:08:16 -0400

    apply ball movement and give a ball speed variable

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -6,17 +6,32 @@ const GameCanvas = () => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
+        let ballX = 75;
+        let ballSpeedX = 5;
+        let ballY = 75;
+
         const canvas = canvasRef.current;
         const canvasContext = canvas.getContext('2d');
-        canvasContext.fillStyle = '#000000';
-        canvasContext.fillRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
 
-        canvasContext.fillStyle = '#FFF';
-        canvasContext.beginPath();
-        canvasContext.arc(100, 100, 10, 0, Math.PI*2, true);
-        canvasContext.fill();
+        const updateAll = () => {
+            ballX += ballSpeedX;
+
+            canvasContext.fillStyle = '#000000';
+            canvasContext.fillRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
+
+            canvasContext.fillStyle = '#FFF';
+            canvasContext.beginPath();
+            canvasContext.arc(ballX, 100, 10, 0, Math.PI*2, true);
+            canvasContext.fill();
+          }
+
+        const framesPerSecond = 30;
+
+        setInterval(updateAll, 1000/framesPerSecond);
 
       }, [])
+
+
 
     return (
         <div>

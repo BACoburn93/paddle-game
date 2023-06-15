0fd137f4623e93b0ae0e32b1f0a20e9263d1fa4d
Brandon Coburn <bcoburn1@frontiernet.net>
2023-06-15 00:10:32 -0400

    have the ball color change when hitting an edge

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -11,6 +11,10 @@ const GameCanvas = () => {
         let ballY = 75;
         let ballSpeedY = 10;
 
+        let ballColor = 0;
+
+        let ballSize = 10;
+
         const canvas = canvasRef.current;
         const canvasContext = canvas.getContext('2d');
 
@@ -18,36 +22,46 @@ const GameCanvas = () => {
             ballX += ballSpeedX;
             ballY += ballSpeedY;
 
-            if(ballX > canvas.width) {
+            const colors = ['red', 'orange', 'yellow', 'blue', 'green', 'violet', 'white'];
+
+            if (ballX >= canvas.width - ballSize) {
                 ballSpeedX = -ballSpeedX;
+                if (ballColor < colors.length - 1) ballColor++;
+                else ballColor = 0;
             }
 
-            if(ballX < 0) {
+            if (ballX <= ballSize) {
                 ballSpeedX = -ballSpeedX;
+                if (ballColor < colors.length - 1) ballColor++;
+                else ballColor = 0;
             }
 
-            if(ballY > canvas.height) {
+            if (ballY >= canvas.height - ballSize) {
                 ballSpeedY = -ballSpeedY;
+                if (ballColor < colors.length - 1) ballColor++;
+                else ballColor = 0;
             }
 
-            if(ballY < 0) {
+            if (ballY <= ballSize) {
                 ballSpeedY = -ballSpeedY;
+                if (ballColor < colors.length - 1) ballColor++;
+                else ballColor = 0;
             }
 
             canvasContext.fillStyle = '#000000';
             canvasContext.fillRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
 
-            canvasContext.fillStyle = '#FFF';
+            canvasContext.fillStyle = colors[ballColor];
             canvasContext.beginPath();
-            canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true);
+            canvasContext.arc(ballX, ballY, ballSize, 0, Math.PI * 2, true);
             canvasContext.fill();
-          }
+        }
 
         const framesPerSecond = 30;
 
-        setInterval(updateAll, 1000/framesPerSecond);
+        setInterval(updateAll, 1000 / framesPerSecond);
 
-      }, [])
+    }, [])
 
 
 

4db59334f1c39e19c8a5df4beb3c8f7467a2e677
Brandon Coburn <bcoburn1@frontiernet.net>
2023-06-15 04:25:15 -0400

    create paddle and write ball reset functionality

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -12,42 +12,62 @@ const GameCanvas = () => {
         let ballSpeedY = 10;
 
         let ballColor = 0;
-
         let ballSize = 10;
+
+        const PADDLE_WIDTH = 100;
+        const PADDLE_THICKNESS = 10;
+        let paddleX = 400;
 
         const canvas = canvasRef.current;
         const canvasContext = canvas.getContext('2d');
 
+        const updateMousePos = (e) => {
+            let rect = canvas.getBoundingClientRect();
+            let root = document.documentElement;
+
+            let mouseX = e.clientX - rect.left - root.scrollLeft;
+            // let mouseY = e.clientY - rect.top - root.scrollTop;
+
+            paddleX = mouseX - PADDLE_WIDTH / 2;
+        }
+
         const colors = ['red', 'orange', 'yellow', 'blue', 'green', 'violet', 'white'];
+
+        canvas.addEventListener('mousemove', updateMousePos);
 
         const updateAll = () => {
             moveAll();
             drawAll();
         }
 
+        const ballReset = () => {
+            ballX = canvas.width / 2;
+            ballY = canvas.height / 2;
+        }
+
         const moveAll = () => {
             ballX += ballSpeedX;
             ballY += ballSpeedY;
 
-            if (ballX >= canvas.width - ballSize) {
+            if (ballX >= canvas.width - ballSize) { // right
                 ballSpeedX = -ballSpeedX;
                 if (ballColor < colors.length - 1) ballColor++;
                 else ballColor = 0;
             }
 
-            if (ballX <= ballSize) {
+            if (ballX <= ballSize) { // left
                 ballSpeedX = -ballSpeedX;
                 if (ballColor < colors.length - 1) ballColor++;
                 else ballColor = 0;
             }
 
-            if (ballY >= canvas.height - ballSize) {
+            if (ballY >= canvas.height - ballSize) { // bottom
                 ballSpeedY = -ballSpeedY;
                 if (ballColor < colors.length - 1) ballColor++;
                 else ballColor = 0;
             }
 
-            if (ballY <= ballSize) {
+            if (ballY <= ballSize) { // top
                 ballSpeedY = -ballSpeedY;
                 if (ballColor < colors.length - 1) ballColor++;
                 else ballColor = 0;
@@ -55,8 +75,11 @@ const GameCanvas = () => {
         }
 
         const drawAll = () => {
-            colorRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height, '#000000')
-            colorCircle(ballX, ballY, ballSize, colors[ballColor])
+            colorRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height, '#000000');
+
+            colorCircle(ballX, ballY, ballSize, colors[ballColor]);
+
+            colorRect(paddleX, canvas.height - PADDLE_THICKNESS, PADDLE_WIDTH, PADDLE_THICKNESS, '#fff');
         }
 
         const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {

219de0f1a788df09f6eef7462b0e39816a9391dc
Brandon Coburn <bcoburn1@frontiernet.net>
2023-06-18 13:49:19 -0400

    fix bug where game start renders twice

--- src/components/GameCanvas/game-canvas.component.jsx
+++ src/components/GameCanvas/game-canvas.component.jsx
@@ -4,6 +4,7 @@ import './game-canvas.styles.scss';
 
 const GameCanvas = () => {
     const canvasRef = useRef(null);
+    let gameStart = useRef(false);
 
     const CANVAS_WIDTH = 800;
     const CANVAS_HEIGHT = 600;
@@ -14,159 +15,166 @@ const GameCanvas = () => {
     const brickGrid = useMemo(() => new Array(BRICK_COLS * BRICK_ROWS), []);
 
     useEffect(() => {
-        console.log('RESETTING');
-        let canvas, canvasContext;
-        let mouseX = 0;
-        let mouseY = 0;
+        if (!gameStart.current) {
+            gameStart.current = true;
+            let canvas, canvasContext;
+            let mouseX = 0;
+            let mouseY = 0;
 
-        let ballX = 75;
-        let ballSpeedX = 4;
-        let ballY = 75;
-        let ballSpeedY = 8;
+            let ballX = 75;
+            let ballSpeedX = 5;
+            let ballY = 75;
+            let ballSpeedY = 10;
 
-        const BRICK_W = CANVAS_WIDTH / BRICK_COLS;
-        const BRICK_H = 20;
-        const BRICK_GAP = 2;
+            const BRICK_W = CANVAS_WIDTH / BRICK_COLS;
+            const BRICK_H = 20;
+            const BRICK_GAP = 2;
 
-        let ballColor = 0;
-        let ballSize = 10;
+            let ballColor = 0;
+            let ballSize = 10;
 
-        canvas = canvasRef.current;
-        canvasContext = canvas.getContext('2d');
+            canvas = canvasRef.current;
+            canvasContext = canvas.getContext('2d');
 
-        const PADDLE_WIDTH = 100;
-        const PADDLE_THICKNESS = 10;
-        const PADDLE_DIST_FROM_EDGE = canvas.height / 10;
-        let paddleX = 400;
+            const PADDLE_WIDTH = 100;
+            const PADDLE_THICKNESS = 10;
+            const PADDLE_DIST_FROM_EDGE = canvas.height / 10;
+            let paddleX = 400;
 
-        const updateMousePos = (e) => {
-            let rect = canvas.getBoundingClientRect();
-            let root = document.documentElement;
+            const updateMousePos = (e) => {
+                let rect = canvas.getBoundingClientRect();
+                let root = document.documentElement;
 
-            mouseX = e.clientX - rect.left - root.scrollLeft;
-            mouseY = e.clientY - rect.top - root.scrollTop;
+                mouseX = e.clientX - rect.left - root.scrollLeft;
+                mouseY = e.clientY - rect.top - root.scrollTop;
 
-            paddleX = mouseX - PADDLE_WIDTH / 2;
-        }
-
-        const colors = ['red', 'orange', 'yellow', 'blue', 'green', 'violet', 'white'];
-
-        canvas.addEventListener('mousemove', updateMousePos);
-
-        const updateAll = () => {
-            moveAll();
-            drawAll();
-        }
-
-        const ballReset = () => {
-            ballX = canvas.width / 2;
-            ballY = canvas.height / 2;
-        }
-
-        const moveAll = () => {
-            ballX += ballSpeedX;
-            ballY += ballSpeedY;
-
-            if (ballX >= canvas.width - 5) { // right
-                ballSpeedX = -ballSpeedX;
-                if (ballColor < colors.length - 1) ballColor++;
-                else ballColor = 0;
+                paddleX = mouseX - PADDLE_WIDTH / 2;
             }
 
-            if (ballX <= 5) { // left
-                ballSpeedX = -ballSpeedX;
-                if (ballColor < colors.length - 1) ballColor++;
-                else ballColor = 0;
+            const colors = ['red', 'orange', 'yellow', 'blue', 'green', 'violet', 'white'];
+
+            canvas.addEventListener('mousemove', updateMousePos);
+
+            const updateAll = () => {
+                moveAll();
+                drawAll();
             }
 
-            if (ballY >= canvas.height - 5) { // bottom
-                ballReset();
+            const ballReset = () => {
+                ballX = canvas.width / 2;
+                ballY = canvas.height / 2;
             }
 
-            if (ballY <= 5) { // top
-                ballSpeedY = -ballSpeedY;
-                if (ballColor < colors.length - 1) ballColor++;
-                else ballColor = 0;
+            const moveAll = () => {
+                ballX += ballSpeedX;
+                ballY += ballSpeedY;
+
+                if (ballX >= canvas.width - 5) { // right
+                    ballSpeedX = -ballSpeedX;
+                    if (ballColor < colors.length - 1) ballColor++;
+                    else ballColor = 0;
+                }
+
+                if (ballX <= 5) { // left
+                    ballSpeedX = -ballSpeedX;
+                    if (ballColor < colors.length - 1) ballColor++;
+                    else ballColor = 0;
+                }
+
+                if (ballY >= canvas.height - 5) { // bottom
+                    ballReset();
+                }
+
+                if (ballY <= 5) { // top
+                    ballSpeedY = -ballSpeedY;
+                    if (ballColor < colors.length - 1) ballColor++;
+                    else ballColor = 0;
+                }
+
+                let ballBrickCol = Math.floor(ballX / BRICK_W);
+                let ballBrickRow = Math.floor(ballY / BRICK_H);
+                let brickIndexUnderBall = rowCallToArrayIndex(ballBrickCol, ballBrickRow)
+
+                if (ballBrickCol >= 0 && ballBrickCol < BRICK_COLS &&
+                    ballBrickRow >= 0 && ballBrickRow < BRICK_ROWS) {
+                    if (brickGrid[brickIndexUnderBall]) {
+                        brickGrid[brickIndexUnderBall] = false;
+                        ballSpeedY = -ballSpeedY
+                    }
+
+                }
+
+                let paddleTopEdgeY = canvas.height - PADDLE_DIST_FROM_EDGE;
+                let paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS;
+                let paddleLeftEdgeX = paddleX;
+                let paddleRightEdgeX = paddleLeftEdgeX + PADDLE_WIDTH;
+
+                if (ballY > paddleTopEdgeY - PADDLE_THICKNESS && // below top of paddle
+                    ballY < paddleBottomEdgeY && // above bottom of paddle
+                    ballX > paddleLeftEdgeX && // right of left of paddle
+                    ballX < paddleRightEdgeX) { // left of right of paddle
+
+                    ballSpeedY = -ballSpeedY;
+
+                    let centerOfPaddleX = paddleX + PADDLE_WIDTH / 2;
+                    let ballDistFromPaddleCenterX = ballX - centerOfPaddleX;
+                    ballSpeedX = ballDistFromPaddleCenterX * 0.38;
+                }
             }
 
-            let ballBrickCol = Math.floor(ballX / BRICK_W);
-            let ballBrickRow = Math.floor(ballY / BRICK_H);
-            let brickIndexUnderBall = rowCallToArrayIndex(ballBrickCol, ballBrickRow)
+            const drawAll = () => {
+                colorRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height, '#000000');
 
-            if (brickIndexUnderBall >= 0 && brickIndexUnderBall < BRICK_COLS * BRICK_ROWS) {
-                brickGrid[brickIndexUnderBall] = false;
+                colorCircle(ballX, ballY, ballSize, colors[ballColor]);
+
+                colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS, colors[ballColor]);
+
+                drawBricks();
+
+                // colorText(`${mouseBrickCol}, ${mouseBrickRow}: ${brickIndexUnderMouse}`, mouseX, mouseY, colors[ballColor]);
             }
 
-            let paddleTopEdgeY = canvas.height - PADDLE_DIST_FROM_EDGE;
-            let paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS;
-            let paddleLeftEdgeX = paddleX;
-            let paddleRightEdgeX = paddleLeftEdgeX + PADDLE_WIDTH;
-
-            if (ballY > paddleTopEdgeY - PADDLE_THICKNESS && // below top of paddle
-                ballY < paddleBottomEdgeY && // above bottom of paddle
-                ballX > paddleLeftEdgeX && // right of left of paddle
-                ballX < paddleRightEdgeX) { // left of right of paddle
-
-                ballSpeedY = -ballSpeedY;
-
-                let centerOfPaddleX = paddleX + PADDLE_WIDTH / 2;
-                let ballDistFromPaddleCenterX = ballX - centerOfPaddleX;
-                ballSpeedX = ballDistFromPaddleCenterX * 0.38;
+            const rowCallToArrayIndex = (col, row) => {
+                return col + BRICK_COLS * row;
             }
-        }
 
-        const drawAll = () => {
-            colorRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height, '#000000');
+            const drawBricks = () => {
+                for (let brickRow = 0; brickRow < BRICK_ROWS; brickRow++) {
+                    for (let brickCol = 0; brickCol < BRICK_COLS; brickCol++) {
 
-            colorCircle(ballX, ballY, ballSize, colors[ballColor]);
+                        let arrayIndex = rowCallToArrayIndex(brickCol, brickRow);
 
-            colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS, colors[ballColor]);
-
-            drawBricks();
-
-            // colorText(`${mouseBrickCol}, ${mouseBrickRow}: ${brickIndexUnderMouse}`, mouseX, mouseY, colors[ballColor]);
-        }
-
-        const rowCallToArrayIndex = (col, row) => {
-            return col + BRICK_COLS * row;
-        }
-
-        const drawBricks = () => {
-            for (let brickRow = 0; brickRow < BRICK_ROWS; brickRow++) {
-                for (let brickCol = 0; brickCol < BRICK_COLS; brickCol++) {
-
-                    let arrayIndex = rowCallToArrayIndex(brickCol, brickRow);
-
-                    if (brickGrid[arrayIndex]) {
-                        colorRect(BRICK_W * brickCol, BRICK_H * brickRow, BRICK_W - BRICK_GAP, BRICK_H - BRICK_GAP, colors[ballColor]);
+                        if (brickGrid[arrayIndex]) {
+                            colorRect(BRICK_W * brickCol, BRICK_H * brickRow, BRICK_W - BRICK_GAP, BRICK_H - BRICK_GAP, colors[ballColor]);
+                        }
                     }
                 }
             }
+
+            const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {
+                canvasContext.fillStyle = fillColor;
+                canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+            }
+
+            const colorCircle = (centerX, centerY, radius, fillColor) => {
+                canvasContext.fillStyle = fillColor;
+                canvasContext.beginPath();
+                canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+                canvasContext.fill();
+            }
+
+            const colorText = (showWords, textX, textY, fillColor) => {
+                canvasContext.fillStyle = fillColor;
+                canvasContext.fillText(showWords, textX, textY);
+            }
+
+            const framesPerSecond = 30;
+
+            setInterval(updateAll, 1000 / framesPerSecond);
+
+            // brickReset();
         }
-
-        const colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {
-            canvasContext.fillStyle = fillColor;
-            canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
-        }
-
-        const colorCircle = (centerX, centerY, radius, fillColor) => {
-            canvasContext.fillStyle = fillColor;
-            canvasContext.beginPath();
-            canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-            canvasContext.fill();
-        }
-
-        const colorText = (showWords, textX, textY, fillColor) => {
-            canvasContext.fillStyle = fillColor;
-            canvasContext.fillText(showWords, textX, textY);
-        }
-
-        const framesPerSecond = 30;
-
-        setInterval(updateAll, 1000 / framesPerSecond);
-
-        // brickReset();
-    }, [brickGrid])
+    }, [brickGrid, gameStart])
 
     useEffect(() => {
         const brickReset = () => {
